7305e9b59d8d4f85980f2622ad2fd66c80dec591
softprops <d.tangren@gmail.com>
2011-10-15 13:39:25 -0400

    updated version defs

--- project/build.scala
+++ project/build.scala
@@ -4,13 +4,17 @@ import Keys._
 object Build extends sbt.Build {
   import ls.Plugin._
 
+  def lsBuildSettings: Seq[Setting[_]] = lsSettings ++ Seq(
+    (LsKeys.lsHost in Ls) := "http://localhost:5000"
+  )
+
   def commonSettings: Seq[Setting[_]] =
     Defaults.defaultSettings ++ Seq(
       organization := "me.lessis",
-      version := "0.1.3"
-    ) ++ lsSettings ++ Seq(
-      (LsKeys.lsHost in Ls) := "http://localhost:5000"
-    )
+      version := "0.1.3",
+      crossScalaVersions := Seq("2.8.0", "2.8.1", "2.8.2",
+                              "2.9.0", "2.9.0-1", "2.9.1")
+    ) ++ lsBuildSettings
 
   lazy val root = Project("root", file("."),
                            settings = commonSettings).aggregate(a, b, c)
@@ -31,6 +35,10 @@ object Build extends sbt.Build {
                         (LsKeys.tags in Ls) := Seq("http", "web", "unfiltered"),
                         (description in Ls) :=
                           "Web 4.0, possibly even 5",
+                        (homepage in Ls) := Some(url("http://unfiltered.databinder.net/Unfiltered.html")),
+                        (licenses in Ls) := Seq(
+                          ("MIT", url("https://github.com/unfiltered/unfiltered/blob/master/LICENSE"))
+                         ),
                         libraryDependencies += "net.databinder" %% "unfiltered-netty-server" % "0.5.0"
                       ))
 
