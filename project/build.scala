e80c6ffc8f90d4d4c67c894bf91aa2abb6426725
softprops <d.tangren@gmail.com>
2011-10-31 23:22:36 -0400

    testing new format

--- project/build.scala
+++ project/build.scala
@@ -11,7 +11,7 @@ object Build extends sbt.Build {
   def commonSettings: Seq[Setting[_]] =
     Defaults.defaultSettings ++ Seq(
       organization := "me.lessis",
-      version := "0.1.3",
+      version := "0.1.1",
       crossScalaVersions := Seq("2.8.0", "2.8.1", "2.8.2",
                               "2.9.0", "2.9.0-1", "2.9.1")
     ) ++ lsBuildSettings
@@ -29,15 +29,15 @@ object Build extends sbt.Build {
 
   lazy val b = Project("b", file("b"),
                       settings = commonSettings ++ Seq(
-                        name := "unfiltered-netty-server",
+                        name := "ba-ba-boom",
                         (homepage in Ls) := Some(new java.net.URL(
-                          "http://unfiltered.databinder.net/Unfiltered.html")),
-                        (LsKeys.tags in Ls) := Seq("http", "web", "unfiltered"),
+                          "http://somesite.com/ba-ba-boom")),
+                        (LsKeys.tags in Ls) := Seq("explosive"),
                         (description in Ls) :=
-                          "Web 4.0, possibly even 5",
-                        (homepage in Ls) := Some(url("http://unfiltered.databinder.net/Unfiltered.html")),
+                          "This library is not allowed to enter traffic tunnels",
+                        (homepage in Ls) := Some(url("http://somesite.com")),
                         (licenses in Ls) := Seq(
-                          ("MIT", url("https://github.com/unfiltered/unfiltered/blob/master/LICENSE"))
+                          ("MIT", url("https://somesite.com/LICENSE"))
                          ),
                         libraryDependencies += "net.databinder" %% "unfiltered-netty-server" % "0.5.0"
                       ))
